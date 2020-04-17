9b6c8c7d620211de2b1dd3b9574aae0f8b86a4b3
devajana <devajana@andrew.cmu.edu>
2020-04-17 02:29:58 -0700

    Persist url count not working

--- index.js
+++ index.js
@@ -1,52 +1,74 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-const COOKIE_NAME = 'variant'
-let response
+
+const COOKIE_NAME = 'Variant'
+
+var counter = 0
+let count1 = 0
+let count2 = 0
 async function handleRequest(request) {
+  let res
   if (request.method === 'GET') {
+    console.log("Counter: " + ++counter)
     let cookieValue = getCookie(request, COOKIE_NAME)
-    if (cookieValue != null) {
-      response = new Response('cookie present', { status: 200 })
+    if (cookieValue) {
+      res = await fetch(cookieValue)
+      res = await new Response(res.body)
+      res.headers.set('Set-Cookie', setCookie(cookieValue))
     } else {
-      let respons = await generate(request, response)
-      response = Response.redirect(respons)
+      res = await generate(request)
     }
+    res = new HTMLRewriter().on('title', new AttributeHandler("Testing"+counter)).transform(res)
+    res = new HTMLRewriter().on('h1#title', new AttributeHandler("Variant Viewing")).transform(res)
+    res = new HTMLRewriter().on('p#description', new AttributeHandler("This is the picked variant for you!")).transform(res)
+    res = new HTMLRewriter().on('a#url', new AttributeHandler("www.google.com")).transform(res)
   } else {
-    response = new Response('Expected GET', { status: 500 })
+    return new Response('Expected GET', { status: 500 })
   }
-  return response
+  console.log("----------------------------------------")
+  return res
+}
+
+function setCookie(value) {
+  return COOKIE_NAME + '=' + value + '; path=/; Domain=.devajana.workers.dev; HttpOnly; SameSite=Lax; Secure'
+}
+
+function getCookie(request, name) {
+  let result = null
+  let cookieString = request.headers.get('Cookie')
+  // console.log("Got cookie: "+cookieString)
+  if (cookieString) {
+    // console.log("Incoming cookie: "+ cookieString)
+    let cookies = cookieString.split(';')
+    cookies.forEach(cookie => {
+      let cookieName = cookie.split('=')[0].trim()
+      if (cookieName === name) {
+        let cookieVal = cookie.split('=')[1]
+        result = cookieVal
+      }
+    })
+  }
+  // console.log("Cookie Result: "+result)
+  return result
 }
 
 async function generate(request) {
   const links = await getLinks()
-  let fooValue = foo()
-  if (fooValue === 'false') {
-    setCookie(0)
-    return links[0];
+  let link
+  if (count1 < count2) {
+    count1++;
+    console.log("count1: "+count1);
+    link = links[0]
+  } else {
+    count2++
+    console.log("count2: "+count2);
+    link = links[1]
   }
-  if (fooValue === 'true') {
-    setCookie(1)
-    return links[1];
-  }
-}
-
-function foo() {
-  if( typeof foo.counter === 'undefined' ) {
-    foo.counter = 'false';
-  } else
-  if( foo.counter === 'false' ) {
-    foo.counter = 'true';
-  } else
-  if( foo.counter === 'true' ) {
-    foo.counter = 'false';
-  }
-  console.log(foo.counter)
-  return foo.counter
+  var response = await fetch(link)
+  var res = await new Response(response.body)
+  res.headers.set('Set-Cookie', setCookie(link))
+  return res
 }
 
 async function getLinks(fooValue) {
@@ -59,27 +81,24 @@ async function getLinks(fooValue) {
   const res = await fetch('https://cfw-takehome.developers.workers.dev/api/variants', init )
   var resjson = await res.text()
   const jsonObj = JSON.parse(resjson)
-  console.log(jsonObj.variants)
   return jsonObj.variants
 }
 
-function setCookie(value) {
-  const created_cookie = `variant=`+ value +`; Expires=Wed, 21 Oct 2020 07:28:00 GMT; Path='/';`
-  response.headers.set('Set-Cookie', created_cookie)
+class ElementHandler {
+
+  element(element) {
+    console.log(element)
+    element.remove()
+  }
 }
 
-async function getCookie(request, name) {
-  let result = null
-  let cookieString = request.headers.get('Cookie')
-  if (cookieString) {
-    let cookies = cookieString.split(';')
-    cookies.forEach(cookie => {
-      let cookieName = cookie.split('=')[0].trim()
-      if (cookieName === name) {
-        let cookieVal = cookie.split('=')[1]
-        result = cookieVal
-      }
-    })
+class AttributeHandler {
+  constructor(attributeName) {
+    this.attributeName = attributeName
   }
-  return result
+
+  element(element) {
+    element.setInnerContent(this.attributeName)
+
+  }
 }
