36b5a1b593750e52c2571b108481805441d0df4c
devajana <devajana@andrew.cmu.edu>
2020-04-22 01:11:36 -0700

    opting different approach

--- index.js
+++ index.js
@@ -41,37 +41,23 @@ async function handleRequest(request) {
 
 async function generate(request) {
   const links = await getLinks()
-  let fooValue = foo()
-  if (fooValue === 'false') {
-    setCookie(0)
-    return links[0];
-  }
-  if (fooValue === 'true') {
-    setCookie(1)
-    return links[1];
+  var links
+  if (count1 < count2) {
+    count1++
+    link = links[0]
+  } else {
+    count2++
+    link = links[1]
   }
 }
 
-async function getLinks(fooValue) {
-  const init = {
-    method: 'GET',
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8'
-    }
-  }
-  const res = await fetch('https://cfw-takehome.developers.workers.dev/api/variants', init )
-  var resjson = await res.text()
-  const jsonObj = JSON.parse(resjson)
-  console.log(jsonObj.variants)
-  return jsonObj.variants
-}
 
 function setCookie(value) {
   const created_cookie = `variant=`+ value +`; Expires=Wed, 21 Oct 2020 07:28:00 GMT; Path='/';`
   response.headers.set('Set-Cookie', created_cookie)
 }
 
-async function getCookie(request, name) {
+function getCookie(request, name) {
   let result = null
   let cookieString = request.headers.get('Cookie')
   if (cookieString) {
